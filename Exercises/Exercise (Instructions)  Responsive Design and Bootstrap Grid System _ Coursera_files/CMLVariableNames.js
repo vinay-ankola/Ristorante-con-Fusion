1ebabe2a2302de84fff228d2ae23a00ddc639a25
Vinay Ankola <vinay.ankola@gmail.com>
2015-12-08 12:30:21 -0500

    conFusion 1.0

---
+++ Exercises/Exercise (Instructions)  Responsive Design and Bootstrap Grid System _ Coursera_files/CMLVariableNames.js
@@ -0,0 +1,1 @@
+"use strict";define("bundles/CMLConverter/CMLVariableNames",["require","exports","module"],function(require,exports,module){module.exports={NAME:"NAME",USER_ID:"USER_ID"}});
---
+++ Exercises/Navigation and Navigation Bar  Additional Resources _ Coursera_files/supplement_003.js
@@ -0,0 +1,1 @@
+"use strict";define("pages/open-course/supplement/views/nls/supplement",["require","exports","module"],function(require,exports,module){module.exports={}});
